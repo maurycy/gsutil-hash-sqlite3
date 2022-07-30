ad1eaa84e6ddf16988b26a7caba6c465dd946a89
maurycy <5383+maurycy@users.noreply.github.com>
2022-07-30 04:18:05 +0200

    logging, support large files, handle exceptions

--- local.py
+++ local.py
@@ -1,18 +1,30 @@
 import argparse
 import base64
 import hashlib
+import logging
 import os
 import sqlite3
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directories', metavar='directory', type=str, nargs='+')
 parser.add_argument('--database', type=str, default='db.sqlite3')
+parser.add_argument('--verbose', action=argparse.BooleanOptionalAction)
 
 
 def hash(path):
     # https://github.com/GoogleCloudPlatform/gsutil/blob/db22c6cf44e4f58a56864f0a6f9bcdf868a3c156/gslib/utils/hashing_helper.py#L376
     # .encode('base64').strip()
-    return base64.b64encode(hashlib.md5(open(path, 'rb').read()).digest()).decode('ascii')
+    md5 = hashlib.md5()
+
+    with open(path, 'rb') as f:
+        while True:
+            data = f.read(8192)
+            if not data:
+                break
+            md5.update(data)
+
+    return base64.b64encode(md5.digest()).decode('ascii')
 
 
 def files(dir):
@@ -22,12 +34,17 @@ def files(dir):
         elif entry.is_dir():
             yield from files(entry.path)
         else:
-            # raise Exception('Unknown entry type: {}'.format(entry.path))
+            logging.warning('Unknown entry type: {}'.format(entry.path))
             continue
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level='DEBUG')
+
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
     con = sqlite3.connect(args.database)
     cur = con.cursor()
@@ -42,9 +59,19 @@ CREATE TABLE IF NOT EXISTS files (
     con.commit()
 
     for directory in args.directories:
-      for path in files(os.path.abspath(directory)):
-        cur.execute('''INSERT INTO files (path, hash) VALUES (:path, :hash)''', {
-                    'path': path, 'hash': hash(path)})
-        con.commit()
+        for path in files(os.path.abspath(directory)):
+            logging.debug(path)
 
-        print(path)
+            h = None
+
+            try:
+                h = hash(path)
+            except Exception as e:
+                logging.warning('Failed to hash {}: {}'.format(path, str(e)))
+                continue
+
+            cur.execute('''INSERT INTO files (path, hash) VALUES (:path, :hash)''', {
+                        'path': path, 'hash': hash(path)})
+            con.commit()
+
+            logging.info("{} {}".format(path, h))
