2e9b22e91618e9462ac59d6dff5d92756bf1e0b6
maurycy <5383+maurycy@users.noreply.github.com>
2022-07-31 05:47:27 +0200

    cmd: stub

---
+++ cmd.py
@@ -0,0 +1,37 @@
+import argparse
+import base64
+import hashlib
+import logging
+import os
+import sqlite3
+import sys
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['count'])
+    parser.add_argument('--database', type=str, default='db.sqlite3')
+    parser.add_argument('--include', metavar='substring', type=str)
+
+    args = parser.parse_args()
+
+    logging.basicConfig(stream=sys.stdout,
+                        format='%(levelname)s %(asctime)s - %(message)s',
+                        level='INFO')
+
+    con = sqlite3.connect(args.database)
+    cur = con.cursor()
+
+    match args.command:
+        case 'count':
+            if args.substring:
+                res = cur.execute(
+                    'SELECT COUNT() FROM files WHERE path LIKE :pattern',
+                    {'pattern': ''.join(['%', args.substring, '%']),})
+                print(cur.fetchone()[0])
+            else:
+                res = cur.execute('SELECT COUNT() FROM files')
+                print(res.fetchone()[0])
+
+    con.close()
